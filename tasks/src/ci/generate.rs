0a2bf46f54d094df0648bad198f2427421fb3437
David Karrick <the.white.wolf.is.1337@gmail.com>
2023-03-31 23:59:58 -0600

    Chore(CI): Tweak summary UI

--- tasks/src/ci/generate.rs
+++ tasks/src/ci/generate.rs
@@ -2,6 +2,8 @@ use super::parse_data::{
     CoverageFileAnalysis, CoverageResult, NextestResult, NextestSuite, NextestTest,
 };
 
+const PASS_SVG: &str = r##"<svg width="16" height="16" style="margin-top: 2px" class="octicon octicon-check-circle-fill color-fg-success" aria-label="completed successfully" viewBox="0 0 16 16" version="1.1" role="img"><path d="M8 16A8 8 0 1 1 8 0a8 8 0 0 1 0 16Zm3.78-9.72a.751.751 0 0 0-.018-1.042.751.751 0 0 0-1.042-.018L6.75 9.19 5.28 7.72a.751.751 0 0 0-1.042.018.751.751 0 0 0-.018 1.042l2 2a.75.75 0 0 0 1.06 0Z"></path></svg>"##;
+const FAIL_SVG: &str = r##"<svg width="16" height="16" style="margin-top: 2px" class="octicon octicon-x-circle-fill color-fg-danger" aria-label="failed" viewBox="0 0 16 16" version="1.1" role="img"><path d="M2.343 13.657A8 8 0 1 1 13.658 2.343 8 8 0 0 1 2.343 13.657ZM6.03 4.97a.751.751 0 0 0-1.042.018.751.751 0 0 0-.018 1.042L6.94 8 4.97 9.97a.749.749 0 0 0 .326 1.275.749.749 0 0 0 .734-.215L8 9.06l1.97 1.97a.749.749 0 0 0 1.275-.326.749.749 0 0 0-.215-.734L9.06 8l1.97-1.97a.749.749 0 0 0-.326-1.275.749.749 0 0 0-.734.215L8 6.94Z"></path></svg>"##;
 pub(crate) trait HTMLTable {
     fn to_th(&self) -> String;
     fn to_td(&self) -> String;
@@ -13,7 +15,7 @@ impl HTMLTable for NextestSuite {
     }
     fn to_td(&self) -> String {
         format!(
-            "<tr><td width=\"75%\">{}</td><td>{}s</td><td>{}</td></tr>",
+            "<tr><td width=\"75%\">{}</td><td>{}</td><td>{}</td></tr>",
             self.name,
             self.count - self.failures,
             self.failures
@@ -30,7 +32,7 @@ impl HTMLTable for NextestTest {
             "<tr><td width=\"75%\">{}</td><td>{}s</td><td>{}</td></tr>",
             self.name,
             self.time,
-            if self.failure { "🔴" } else { "🟢" }
+            if self.failure { FAIL_SVG } else { PASS_SVG }
         )
     }
 }
